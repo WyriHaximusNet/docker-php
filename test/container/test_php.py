7ae0a14a55e27dca6612e540e377e052ea183f1c
Cees-Jan Kiewiet <ceesjank@gmail.com>
2019-03-27 11:36:21 +0100

    Escape namespace separator

--- test/container/test_php.py
+++ test/container/test_php.py
@@ -17,3 +17,20 @@ def test_php_pcntl_is_enabled(host):
 
     output = host.run('php -r "exit(function_exists(\'uv_timer_init\') ? 0 : 255);"')
     assert output.rc == 0
+
+@pytest.mark.php_zts
+def test_php_pcntl_is_enabled(host):
+    output = host.run('php -r "exit(class_exists(\'parallel\\Runtime\') ? 0 : 255);"')
+    assert output.rc == 0
+
+    output = host.run('php -r "exit(class_exists(\'parallel\\Future\') ? 0 : 255);"')
+    assert output.rc == 0
+
+@pytest.mark.php_nts
+def test_php_pcntl_is_enabled(host):
+    output = host.run('php -r "exit(class_exists(\'parallel\\Runtime\') ? 0 : 255);"')
+    assert output.rc == 255
+
+    output = host.run('php -r "exit(class_exists(\'parallel\\Future\') ? 0 : 255);"')
+    assert output.rc == 255
+
