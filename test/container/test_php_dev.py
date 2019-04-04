bad979a045d5f201680904ce5789818efb03dff7
Cees-Jan Kiewiet <ceesjank@gmail.com>
2019-04-04 21:35:38 +0200

    Removed tests for removed options

--- test/container/test_php_dev.py
+++ test/container/test_php_dev.py
@@ -10,8 +10,6 @@ def test_configuration_is_effective(host):
     configuration = host.run('php -i').stdout
     
     assert u'expose_php => On => On' in configuration
-    assert u'opcache.validate_timestamps => On => On' in configuration
-    assert u'zend.assertions => 1 => 1' in configuration
 
 @pytest.mark.php_dev
 def test_xdebug_is_loaded(host):
@@ -27,8 +25,6 @@ def test_configuration_is_not_effective(host):
     configuration = host.run('php -i').stdout
     
     assert u'expose_php => Off => Off' in configuration
-    assert u'opcache.validate_timestamps => Off => Off' in configuration
-    assert u'zend.assertions => -1 => -1' in configuration
 
 @pytest.mark.php_no_dev
 def test_xdebug_is_not_loaded(host):
