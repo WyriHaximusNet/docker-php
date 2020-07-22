4a46c7153edbcbdfc626843085fe0b6c4c32f200
Cees-Jan Kiewiet <ceesjank@gmail.com>
2020-07-22 15:50:11 +0200

    Enable opcache on CLI
    This improves stability in more complex systems that use ext-parallel.

--- test/container/test_php_config.py
+++ test/container/test_php_config.py
@@ -5,6 +5,7 @@ def test_configuration_is_effective(host):
     config = get_config(host)
 
     assert u'memory_limit => -1 => -1' in config
+    assert u'opcache.enable_cli => On => On' in config
 
 def get_config(host):
     return host.run('php -i').stdout
