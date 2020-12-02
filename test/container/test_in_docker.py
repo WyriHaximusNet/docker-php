1d933fbf46f0104e396f79ae1842207f64a44089
Cees-Jan Kiewiet <ceesjank@gmail.com>
2020-12-02 20:30:49 +0100

    Test that make works

--- test/container/test_in_docker.py
+++ test/container/test_in_docker.py
@@ -10,3 +10,10 @@ def test_in_docker_file_exists(host):
 def test_in_docker_file_exists(host):
     output = host.run('php -r "exit(file_exists(\'/.you-are-in-a-wyrihaximus.net-php-docker-image-dev\') ? 0 : 255);"')
     assert output.rc == 0
+
+@pytest.mark.php_dev
+def test_make_works(host):
+    output = host.run('make')
+    assert output.stdout == ''
+    assert u'No targets specified and no makefile found.' in output.stderr
+    assert output.rc == 2
