cf317c28ebf9236456493b4dcb45bd4e10e07f66
Cees-Jan Kiewiet <ceesjank@gmail.com>
2021-07-04 00:30:56 +0200

    Provide Alpine and Debian images without version
    Essentially this will provide a latest kind of tag

--- build-php.sh
+++ build-php.sh
@@ -12,7 +12,9 @@ declare -r OS=$4
 
 declare -r VERSION_OS=$5
 
-declare -r VERSION_OS_FROM=$6
+declare -r VERSION_OS_TAG=$6
+
+declare -r VERSION_OS_FROM=$7
 
 # I could create a placeholder like php:x.y-image-alpinex.y in the Dockerfile itself,
 # but I think it wouldn't be a good experience if you try to build the image yourself
@@ -20,7 +22,7 @@ declare -r VERSION_OS_FROM=$6
 declare -r IMAGE_ORIGINAL_TAG="7.[0-9]-${SRC_IMAGE}-${VERSION_OS_FROM}"
 
 declare -r IMAGE_TAG="${VERSION_PHP}-${SRC_IMAGE}-${VERSION_OS}"
-declare -r WYRIHAXIMUSNET_TAG="wyrihaximusnet/php:${VERSION_PHP}-${DST_IMAGE}-${VERSION_OS}"
+declare -r WYRIHAXIMUSNET_TAG="wyrihaximusnet/php:${VERSION_PHP}-${DST_IMAGE}-${VERSION_OS_TAG}"
 declare -r WYRIHAXIMUSNET_TAG_DEV="${WYRIHAXIMUSNET_TAG}-dev"
 declare -r WYRIHAXIMUSNET_TAG_ROOT="${WYRIHAXIMUSNET_TAG}-root"
 declare -r WYRIHAXIMUSNET_TAG_DEV_ROOT="${WYRIHAXIMUSNET_TAG}-dev-root"
--- utils/newer-upstream-images.php
+++ utils/newer-upstream-images.php
@@ -17,32 +17,35 @@ foreach (range(1, 10) as $i) {
 }
 
 $line = [];
-foreach (json_decode(getenv('ALPINE'), true) as $alpine) {
-    foreach (json_decode(getenv('PHP'), true) as $php) {
+foreach (json_decode(getenv('PHP'), true) as $php) {
+    foreach (json_decode(getenv('ALPINE'), true) as $alpine) {
         $name = $php . '-zts-alpine' . $alpine;
 
         if (array_key_exists($name, $upstreamImages)) {
             if (!(array_key_exists($name, $images) && $upstreamImages[$name] < $images[$name])) {
-                $line[] = 'zts-zts-' . $php . '-alpine-alpine' . $alpine . '-alpine3.11';
+                $line[] = 'zts-zts-' . $php . '-alpine' . $alpine . '-alpine-alpine3.11';
             }
         }
         $name = $php . '-cli-alpine' . $alpine;
 
         if (array_key_exists($name, $upstreamImages)) {
             if (!(array_key_exists($name, $images) && $upstreamImages[$name] < $images[$name])) {
-                $line[] = 'cli-nts-' . $php . '-alpine-alpine' . $alpine . '-alpine3.11';
+                $line[] = 'cli-nts-' . $php . '-alpine' . $alpine . '-alpine-alpine3.11';
             }
         }
     }
 }
 
-foreach (json_decode(getenv('DEBIAN'), true) as $debian) {
-    foreach (json_decode(getenv('PHP'), true) as $php) {
+foreach (json_decode(getenv('PHP'), true) as $php) {
+    foreach (json_decode(getenv('DEBIAN'), true) as $debian) {
         $name = $php . '-zts-debian' . $debian;
 
         if (array_key_exists($name, $upstreamImages)) {
             if (!(array_key_exists($name, $images) && $upstreamImages[$name] < $images[$name])) {
                 $line[] = 'zts-zts-' . $php . '-debian-' . $debian . '-buster';
+                if ($debian === 'buster') {
+                    $line[] = 'zts-zts-' . $php . '-debian-' . $debian . '-buster-buster';
+                }
             }
         }
         $name = $php . '-cli-debian' . $debian;
@@ -50,6 +53,9 @@ foreach (json_decode(getenv('DEBIAN'), true) as $debian) {
         if (array_key_exists($name, $upstreamImages)) {
             if (!(array_key_exists($name, $images) && $upstreamImages[$name] < $images[$name])) {
                 $line[] = 'cli-nts-' . $php . '-debian-' . $debian . '-buster';
+                if ($debian === 'buster') {
+                    $line[] = 'cli-nts-' . $php . '-debian-' . $debian . '-buster-buster';
+                }
             }
         }
     }
