53b31f2ea020431281ac339511c84972fdcc7a64
Cees-Jan Kiewiet <ceesjank@gmail.com>
2022-05-13 19:38:01 +0200

    Export exclude list from step

---
+++ utils/exclude-list.php
@@ -0,0 +1,99 @@
+<?php
+
+$excludes = [];
+$excludes[] = [
+    'arch' => 'arm',
+    'image' => 'zts-zts-8.1-8.1-alpine-alpine3.15-alpine3.15-alpine3.11',
+];
+$excludes[] = [
+    'arch' => 'arm64',
+    'image' => 'zts-zts-8.1-8.1-alpine-alpine3.15-alpine3.15-alpine3.11',
+];
+$excludes[] = [
+    'arch' => 'amd64',
+    'image' => 'zts-zts-8.1-8.1-alpine-alpine3.15-alpine3.15-alpine3.11',
+];
+$excludes[] = [
+    'arch' => 'arm',
+    'image' => 'zts-zts-8.1-8.1-alpine-alpine3.14-alpine3.14-alpine3.11',
+];
+$excludes[] = [
+    'arch' => 'arm64',
+    'image' => 'zts-zts-8.1-8.1-alpine-alpine3.14-alpine3.14-alpine3.11',
+];
+$excludes[] = [
+    'arch' => 'amd64',
+    'image' => 'zts-zts-8.1-8.1-alpine-alpine3.14-alpine3.14-alpine3.11',
+];
+$excludes[] = [
+    'arch' => 'arm',
+    'image' => 'zts-zts-8.1-8.1-alpine-alpine3.15-alpine-alpine3.11',
+];
+$excludes[] = [
+    'arch' => 'arm64',
+    'image' => 'zts-zts-8.1-8.1-alpine-alpine3.15-alpine-alpine3.11',
+];
+$excludes[] = [
+    'arch' => 'amd64',
+    'image' => 'zts-zts-8.1-8.1-alpine-alpine3.15-alpine-alpine3.11',
+];
+$excludes[] = [
+    'arch' => 'arm',
+    'image' => 'zts-zts-8.1-8.1-alpine-alpine3.15-alpine3.15-alpine3.11',
+];
+$excludes[] = [
+    'arch' => 'arm',
+    'image' => 'zts-zts-8.1-8.1-alpine-alpine3.15-alpine-alpine3.11',
+];
+$excludes[] = [
+    'arch' => 'arm',
+    'image' => 'zts-zts-8.1-8.1-alpine-alpine3.14-alpine3.14-alpine3.11',
+];
+$excludes[] = [
+    'arch' => 'arm64',
+    'image' => 'zts-zts-8.1-8.1-alpine-alpine3.14-alpine3.14-alpine3.11',
+];
+$excludes[] = [
+    'arch' => 'amd64',
+    'image' => 'zts-zts-8.1-8.1-alpine-alpine3.14-alpine3.14-alpine3.11',
+];
+$excludes[] = [
+    'arch' => 'arm',
+    'image' => 'cli-nts-8.1-8.1-alpine-alpine3.15-alpine-alpine3.11',
+];
+$excludes[] = [
+    'arch' => 'arm',
+    'image' => 'cli-nts-8.1-8.1-alpine-alpine3.15-alpine3.15-alpine3.11',
+];
+$excludes[] = [
+    'arch' => 'arm',
+    'image' => 'cli-nts-8.1-8.1-alpine-alpine3.15-alpine3.15-alpine3.11',
+];
+$excludes[] = [
+    'arch' => 'arm',
+    'image' => 'cli-nts-8.1-8.1-debian-buster-buster-buster',
+];
+$excludes[] = [
+    'arch' => 'arm',
+    'image' => 'cli-nts-8.1-8.1-debian-bullseye-bullseye-buster',
+];
+$excludes[] = [
+    'arch' => 'arm',
+    'image' => 'cli-nts-8.1-8.1-debian-buster-debian-buster',
+];
+$excludes[] = [
+    'arch' => 'arm',
+    'image' => 'zts-zts-8.1-8.1-debian-bullseye-bullseye-buster',
+];
+$excludes[] = [
+    'arch' => 'arm',
+    'image' => 'zts-zts-8.1-8.1-debian-buster-buster-buster',
+];
+$excludes[] = [
+    'arch' => 'arm',
+    'image' => 'zts-zts-8.1-8.1-debian-buster-debian-buster',
+];
+
+
+echo 'Excludes: ', json_encode($excludes), PHP_EOL;
+echo '::set-output name=exclude::', json_encode($excludes), PHP_EOL;;
