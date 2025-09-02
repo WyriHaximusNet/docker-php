26c8e3ac9d22bbb22ef8c15473754f3aa9869c63
Cees-Jan Kiewiet <ceesjank@gmail.com>
2025-09-02 11:16:20 +0200

    Drop GRPC from 8.5 and up
    It's heavy, adding it in retro spec was a mistake as it blocked the event loop.

--- test/container/test_php_otel.py
+++ test/container/test_php_otel.py
@@ -7,10 +7,5 @@ def test_ffs_is_loaded(host):
 
 @pytest.mark.php_nts
 @pytest.mark.php_zts
-def test_grpc_is_loaded(host):
-    assert 'grpc' in host.run('php -m').stdout
-
-@pytest.mark.php_nts
-@pytest.mark.php_zts
 def test_opentelemetry_is_loaded(host):
     assert 'opentelemetry' in host.run('php -m').stdout
